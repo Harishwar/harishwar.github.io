8e701052f941781f29615067154f96b77f4af606
Harishwar <hello@harishwar.com>
2016-04-23 16:31:31 -0700

    Add Unsplash

--- js/init.js
+++ js/init.js
@@ -43,3 +43,26 @@ $('.nav-bg').on({
         $(this).toggle();
     }
 });
+
+/* Unsplash */
+var Unsplash = new UnsplashPhoto();
+var images = [
+    'BYu8ITUWMfc',
+    'vwY2D2Wr4ME',
+    '8-o4p16GzSc',
+    'jwIk4Z3Msi4',
+    'iIg4F2IWbTM',
+    'IY-gY1FK-bo',
+    '63qfL0TciY8',
+    'ywnnwzcdR5o',
+    'DjlKxYFJlTc',
+    'FIKD9t5_5zQ',
+    'ahzw2Tg7IXs',
+    'KxWdfIgQTUE',
+    'RJK6p5MJACQ',
+    'lQqVJkIx1g0',
+    'ULr8dMN6yDY'
+];
+var index = Math.floor(Math.random() * images.length);
+var imageURL = Unsplash.find(images[index]).size(1366, 768).fetch();
+$('body').css('background-image', 'url(' + imageURL + ')');
